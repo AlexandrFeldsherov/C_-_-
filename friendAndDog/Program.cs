86e5d632530891ceb78da55f747051ea8913b4e1
[AlexandrFeldsherov] <[alexandrfeldsherov@mail.ru]>
2022-06-24 20:25:58 +0300

    присвоил значение переменным из консоли

--- friendAndDog/Program.cs
+++ friendAndDog/Program.cs
@@ -3,13 +3,14 @@ int vSecond;
 int vDog;
 int sFriend;
 int t;
-int nDog;
+int nDog=0;
 bool fS=false;
 
-
-
-
-
-
 Console.WriteLine("ведите скорость первого друга ");
-int vFiкst = 
+int vFiкst = Console.ReadLine();
+Console.WriteLine("ведите скорость второго друга ");
+int vSecond = Console.ReadLine();
+Console.WriteLine("ведите скорость собаки, но учти собака быстрее друзей!  ");
+int vDog= Console.ReadLine();
+Console.WriteLine("ведите расстояние между друзей ");
+int sFriend = Console.ReadLine();
